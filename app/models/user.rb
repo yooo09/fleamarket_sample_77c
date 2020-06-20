cc1d56a99f88be1cb1a3c697be215e1cbd72e423
okamayu <mayu.okagawa0511@gmail.com>
2020-06-20 18:10:23 +0900

    user 新規登録、ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :encrypted_password, length: { minimum: 7 }
-  validates :password, confirmation: true
+  validates :password, confirmation: true, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
