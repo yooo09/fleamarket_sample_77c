bedc345ca080736920ff886f0a92fbbf5f385280
yooo09 <65532756+yooo09@users.noreply.github.com>
2020-06-21 14:56:59 +0900

    トップページ商品一覧表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
+  before_action :authenticate_user!, except: [:index]
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_113016) do
+ActiveRecord::Schema.define(version: 2020_06_21_022733) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_06_19_113016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price", null: false
+    t.integer "customer_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
