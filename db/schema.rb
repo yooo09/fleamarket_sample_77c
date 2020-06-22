1d03db886488059543ea52778a91c750181ed21d
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-06-22 11:41:41 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_130250) do
+ActiveRecord::Schema.define(version: 2020_06_21_022733) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
