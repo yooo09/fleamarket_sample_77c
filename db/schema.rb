b2f182928816bc09229565014a861be4679ce41e
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-06-26 20:37:22 +0900

    Revert "Merge pull request #62 from honecha9/migrate_remake_file"
    This reverts commit d49ee2c75ec165846ceb090022d37ebbbb1923c1, reversing
    changes made to 692cdc6e3945fa6bdc34eb93d2aca3ce650fb7d2.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_06_25_050742) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
