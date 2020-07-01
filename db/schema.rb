e40457020b34ff098a7095801d5f1196d6865ba5
sakagu <sakatoshi10416@gmail.com>
2020-07-01 15:54:44 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,14 @@ ActiveRecord::Schema.define(version: 2020_06_30_083354) do
     t.index ["user_id_id"], name: "index_adresses_on_user_id_id"
   end
 
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
     t.bigint "user_id", null: false
@@ -40,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_06_30_083354) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_comments_on_item_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-
   end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
