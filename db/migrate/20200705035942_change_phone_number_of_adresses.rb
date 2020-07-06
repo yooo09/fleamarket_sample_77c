4d10e002a994e0b170b4dd5fd7f4a128d1321eb6
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-07-06 15:06:01 +0900

    add

--- db/migrate/20200705035942_change_phone_number_of_adresses.rb
+++ db/migrate/20200705035942_change_phone_number_of_adresses.rb
@@ -1,6 +1,5 @@
 class ChangePhoneNumberOfAdresses < ActiveRecord::Migration[5.2]
   def up
     change_column :adresses, :phone_number,:string
-    rename_column :adresses, :user_id, :user_id_id
   end
 end
