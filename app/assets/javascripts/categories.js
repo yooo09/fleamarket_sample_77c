1b3dcf2511175cbb197cb270a538a70883b86969
sakagu <sakatoshi10416@gmail.com>
2020-07-06 19:10:32 +0900

    add

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -66,7 +66,7 @@ $(document).on('turbolinks:load', function(){
 
     // 子カテゴリー選択後のイベント
     $('.category__choose').on('change','#child_category', function(){
-      var child_category_id = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
+      var child_category_id = $('#child_category option:selected').val(); //選択された子カテゴリーのidを取得
       console.log(child_category_id);
       if (child_category_id != "---"){ //子カテゴリーが初期値でないことを確認
         $.ajax({
