a3c674d860313d66f4fda956d53f99db3704d5ef
haruna44 <haruhana44@gmail.com>
2020-07-07 14:05:03 +0900

    商品購入機能
    最終commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_07_05_035942) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -75,7 +74,6 @@ ActiveRecord::Schema.define(version: 2020_07_05_035942) do
     t.integer "user_id", null: false
     t.string "brand_id"
     t.integer "category_id"
-    t.integer "customer_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
